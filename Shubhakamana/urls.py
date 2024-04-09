b3fda8bdc936e7b3271931ebec6119ee2decb05f
keshavdahal <keshavdahal160@gmail.com>
2024-04-09 15:41:51 +0545

    Resolve media

--- Shubhakamana/urls.py
+++ Shubhakamana/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin # type: ignore
-from django.urls import path, include # type: ignore
+from django.contrib import admin 
+from django.urls import path, include 
+from django.conf import settings
+from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path(r'^media/(?P<path>).*$', serve,{'document_root':settings.MEDIA_ROOT})
 ]
